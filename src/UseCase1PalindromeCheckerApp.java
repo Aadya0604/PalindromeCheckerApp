7f0de2c2e13493bd47004d30b76755c9ec73c379
Aadya0604 <aadyagarg29@gmail.com>
2026-03-12 19:11:13 +0530

    UC1 Palindrome Checker App

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("=================================");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println("=================================");
+
+        // Application details
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version : 1.0");
+
+        System.out.println("Application Started Successfully!");
+        System.out.println("Ready for Palindrome Checking...");
+
+    }
+}
